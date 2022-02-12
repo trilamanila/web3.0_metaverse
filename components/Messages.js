85504030a4bd478eda8532a81a2b488f6a59d840
Joncin Picol <jctrilamanila@gmail.com>
2022-02-12 04:31:53 -0500

    created live message listener with Moralis

--- components/Messages.js
+++ components/Messages.js
@@ -1,10 +1,28 @@
 import { useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
 import SendMessage from "./SendMessage";
+import Message from "./Message";
+
+//Only show messages from the last 60 minutes
+const MINS_DURATION = 60;
 
 function Messages() {
   const { user } = useMoralis();
   const endOfMessageRef = useRef(null);
+  const { data, loading, error } = useMoralisQuery(
+    "Messages",
+    (query) =>
+      query
+        .ascending("createdAt")
+        .greaterThan(
+          "createdAt",
+          new Date(Date.now() - 1000 * 60 * MINS_DURATION)
+        ),
+    [],
+    {
+      live: true,
+    }
+  );
 
   return (
     <div className="pb-56">
@@ -14,7 +32,11 @@ function Messages() {
           style={{ marginLeft: "auto", marginRight: "auto" }}
         />
       </div>
-      <div>{/*Each Message */}</div>
+      <div className="space-y-10 p-4">
+        {data.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
 
       <div className="flex justify-center">
         <SendMessage endOfMessageRef={endOfMessageRef} />
