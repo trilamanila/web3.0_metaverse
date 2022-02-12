5119e6080054325f241fdb86f01927c6c404f390
Joncin Picol <jctrilamanila@gmail.com>
2022-02-12 00:49:49 -0500

    created header, avatar component, and change username component

---
+++ components/Header.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { useMoralis } from "react-moralis";
+import Avatar from "./Avatar";
+import ChangeUsername from "./ChangeUsername";
+
+function Header() {
+  const { user } = useMoralis();
+
+  return (
+    <div className="text-pink-500">
+      <div className="">
+        <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+            src="/trilamanila.png"
+          />
+        </div>
+
+        <div>
+          <div className="relative h-48 w-48 lg:mx-auto border-pink-500 border-8 rounded-full">
+            <Avatar logoutOnPress />
+          </div>
+
+          <h1 className="text-3xl">Welcome to the Metaverse</h1>
+
+          <h2 className="text-5xl font-bold truncate">{user.getUsername()}</h2>
+
+          <ChangeUsername />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
